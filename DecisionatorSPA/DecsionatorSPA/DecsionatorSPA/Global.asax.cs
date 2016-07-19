922c29f7ec4a509fe69d3daf3e1f4734ed50fed5
Russell Tan <sell_nat@yahoo.com>
2016-07-18 17:57:36 -0700

    Abandoning the asp first way

--- DecisionatorSPA/DecsionatorSPA/DecsionatorSPA/Global.asax.cs
+++ DecisionatorSPA/DecsionatorSPA/DecsionatorSPA/Global.asax.cs
@@ -6,6 +6,8 @@ using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
+using DecsionatorSPA.Models;
+
 namespace DecsionatorSPA
 {
     public class MvcApplication : System.Web.HttpApplication
